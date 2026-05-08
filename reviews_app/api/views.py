f3f190e389c1faad540bfbeb7628d5e0fa81b898
lucas <lucashamacher01@gmail.com>
2026-05-08 14:08:54 +0200

    add: views for reviews

---
+++ reviews_app/api/views.py
@@ -0,0 +1,49 @@
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+
+from reviews_app.models import Review
+from reviews_app.api.serializers import (
+    ReviewSerializer,
+    ReviewCreateSerializer,
+    ReviewUpdateSerializer
+)
+from reviews_app.api.permissions import IsCustomerUser, IsReviewOwner
+
+class ReviewViewSet(viewsets.ModelViewSet):
+
+    ordering_fields = [
+        "rating",
+        "updated_at"
+    ]
+
+    queryset = Review.objects.select_related("business_user", "reviewer")
+
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        queryset = Review.objects.select_related("business_user", "reviewer")
+        business_user_id = self.request.query_params.get("business_user_id")
+        reviewer_id = self.request.query_params.get("reviewer_id")
+        if business_user_id:
+            queryset = queryset.filter(business_user_id=business_user_id)
+        if reviewer_id:
+            queryset = queryset.filter(reviewer_id=reviewer_id)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ReviewCreateSerializer
+        if self.action in ["update", "partial_update"]:
+            return ReviewUpdateSerializer
+        return ReviewSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [IsAuthenticated(), IsCustomerUser()]
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(),IsReviewOwner()]
+        return [IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        reviewer = self.request.user
+        serializer.save(reviewer=reviewer)
