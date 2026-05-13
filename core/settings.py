55e3e0dcec0467a003601af0f3a68a93f68cea4e
lucas <lucashamacher01@gmail.com>
2026-05-13 12:31:15 +0200

    fix: make pagination local for offers

--- core/settings.py
+++ core/settings.py
@@ -135,8 +135,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": (
         "rest_framework.pagination.PageNumberPagination"
-    ),
-    "PAGE_SIZE": 6,
+    )
 }
 
 MEDIA_URL = "/media/"
